8b6a4040c87f173b1475763af9b3df08152b6372
karthikgreninja <35425515+karthikgreninja@users.noreply.github.com>
2019-11-05 14:17:03 +0530

    Add files via upload

--- End Sem/TriangleWithJointSides/app.js
+++ End Sem/TriangleWithJointSides/app.js
@@ -40,7 +40,7 @@ var InitDemo = function () {
 		alert('Your browser does not support WebGL');
 	}
 
-	gl.clearColor(0.75, 0.85, 0.8, 1.0);
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 	//
